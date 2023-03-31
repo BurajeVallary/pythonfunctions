21c4fd10254f9fc55c9fe20d581e4c0bb63255b7
Vallary Buraje <vallaryburaje@gmail.com>
2023-03-31 16:13:32 +0300

    funtions added

---
+++ math_1.py
@@ -0,0 +1,18 @@
+def addition(num1,num2):
+    answer =num1+num2
+    return answer
+
+def subtraction(num1,num2):
+    answer =num1-num2
+    return answer
+
+def divide(num1,num2):
+    answer =num1/num2
+    return answer
+
+def modulas(num1,num2):
+    answer =num1%num2
+    return answer
+
+
+
