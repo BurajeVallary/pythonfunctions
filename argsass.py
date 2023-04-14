89adeeeac98876bcbd7a0e34f942c864fe4dcb0b
Vallary Buraje <vallaryburaje@gmail.com>
2023-04-14 22:07:16 +0300

    changes made

--- argsass.py
+++ argsass.py
@@ -17,11 +17,8 @@ def concatenate_args(*args):
 #A function named concatenate_kwargs that 
 # takes any number of string arguments in keyword
 #  arguments  format and concatenates them into a single string
-def concatenate_kwargs(**kwargs):
-    answer = " "
-    for value in kwargs.values():
-        if isinstance(value,str):
-            answer +=value
-   
-    return answer
-    
+def concatenate_kwargs(**str):
+    strings=""
+    for key,value in str.items():
+        str +=(f"{key},{value}, ")
+    return strings
